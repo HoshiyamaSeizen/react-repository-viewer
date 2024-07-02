878b3a147c9aaaeab7637d2c46d9cbfd581f17cb
HoshiyamaSeizen <nazarafanasyev@gmail.com>
2024-07-03 01:00:32 +0300

    update router

--- src/router.tsx
+++ src/router.tsx
@@ -3,14 +3,19 @@ import RepositoryListPage from './components/RepositoryListPage';
 import ErrorPage from './components/ErrorPage';
 import RepositoryInfoPage from './components/RepositoryInfoPage';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <RepositoryListPage />,
+			errorElement: <ErrorPage />,
+		},
+		{
+			path: '/repository/:owner/:name',
+			element: <RepositoryInfoPage />,
+		},
+	],
 	{
-		path: '/',
-		element: <RepositoryListPage />,
-		errorElement: <ErrorPage />,
-	},
-	{
-		path: '/repository/:owner/:name',
-		element: <RepositoryInfoPage />,
-	},
-]);
+		basename: '/react-repository-viewer/',
+	}
+);
